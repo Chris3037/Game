1c9743a3161fd7460de8df1bffa1ca169bc09478
Chris3037 <Chris3037@gmail.com>
2014-04-22 23:08:27 -0600

    V 2.0

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -12,14 +12,13 @@ namespace Game
         //Public Variables
         static string userName = "";
         static int dir = 1;
+        static char input = ' ';
+        static bool gameOn = true;
 
         static void Main(string[] args)
         {
-            Song();
-            //Welcome();
-            //Object();
-            //Direction();
-            End();
+            Welcome();
+            Engine();
         }
 
         //Welcome Function
@@ -29,23 +28,115 @@ namespace Game
             userName = Console.ReadLine();
             Console.Clear();
             Console.WriteLine("Welcome to the game " + userName);
+            Console.WriteLine("Use the 'A' key and the 'D' key to rotate the object.");
+            Console.WriteLine("That's all you can do...");
+            Engine();
         }
 
-        //Object Function
-        static void Object()
+        //Controls Function
+        static void Controls()
         {
-            Console.WriteLine();
         }
 
         //Direction Function
         static void Direction()
         {
-            Console.WriteLine(dir);
-            if (dir >= 8)
+        }
+
+        //Object Function
+        static void Object()
+        {
+            while (gameOn)
             {
-                dir = 1;
+
+                input = Console.ReadKey().KeyChar;
+                if (input == 'd')
+                {
+                    dir++;
+                }
+                else if (input == 'a')
+                {
+                    dir--;
+                }
+
+                if (dir == 9)
+                {
+                    dir = 1;
+                }
+
+                if (dir == 0)
+                {
+                    dir = 8;
+                }
+                Console.Clear();
+                Console.WriteLine(dir);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                if (dir == 1)
+                {
+                    Console.WriteLine("                                         o");
+                    Console.WriteLine("                                        o o");
+                    Console.WriteLine("                                       o   o");
+                }
+                else if (dir == 2)
+                {
+                    Console.WriteLine("                                          o");
+                    Console.WriteLine("                                        o o");
+                    Console.WriteLine("                                      o   o");
+                }
+                else if (dir == 3)
+                {
+                    Console.WriteLine("                                       o");
+                    Console.WriteLine("                                         o");
+                    Console.WriteLine("                                       o");
+                }
+                else if (dir == 4)
+                {
+                    Console.WriteLine("                                     o   o");
+                    Console.WriteLine("                                       o o");
+                    Console.WriteLine("                                         o");
+                }
+                else if (dir == 5)
+                {
+                    Console.WriteLine("                                      o   o");
+                    Console.WriteLine("                                       o o");
+                    Console.WriteLine("                                        o");
+                }
+                else if (dir == 6)
+                {
+                    Console.WriteLine("                                       o   o");
+                    Console.WriteLine("                                       o o");
+                    Console.WriteLine("                                       o");
+                }
+                else if (dir == 7)
+                {
+                    Console.WriteLine("                                         o");
+                    Console.WriteLine("                                       o");
+                    Console.WriteLine("                                         o");
+                }
+                else if (dir == 8)
+                {
+                    Console.WriteLine("                                        o");
+                    Console.WriteLine("                                        o o");
+                    Console.WriteLine("                                        o   o");
+                }
             }
-            dir++;
+        }
+
+        //Engine Function
+        static void Engine()
+        {
+            while (gameOn)
+            {
+                Object();
+                Thread.Sleep(50);
+            }
         }
 
         //Song Function
